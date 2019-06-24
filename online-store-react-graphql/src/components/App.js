5d12761335c34116dcb7e80622151914a6056fea
Stacey <sachungo1@gmail.com>
2019-06-24 17:20:53 +0300

    Added brands header

--- online-store-react-graphql/src/components/App.js
+++ online-store-react-graphql/src/components/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
+import {
+  Container,
+  Box,
+  Heading
+} from 'gestalt';
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <Container>
+        {/* brand section */}
+        <Box
+          display="flex"
+          justifyContent="center"
+          marginBottom={2}
+        >
+          {/* Brands header */}
+          <Heading color="midnight" size="md">
+            Brew Brands
+          </Heading>
+        </Box>
+      </Container>
+    );
   }
 }
 
