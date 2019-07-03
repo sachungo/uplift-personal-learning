b809db7f0958594303776f3ff893b773d84f0ab9
Stacey <sachungo1@gmail.com>
2019-07-03 11:52:02 +0300

    Created signup form markup and functionality

--- online-store-react-graphql/src/components/Signup.js
+++ online-store-react-graphql/src/components/Signup.js
@@ -1,9 +1,92 @@
 import React from "react";
 
+import {
+  Container,
+  Box,
+  Button,
+  Heading,
+  Text,
+  TextField
+} from "gestalt";
 export default class Signup extends React.Component {
+  state = {
+    username: '',
+    email: '',
+    password: ''
+  };
+
+  handleChange = ({ event, value }) => {
+    event.persist();
+    this.setState({ [event.target.name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    if (this.isFormEmpty(this.state)) return;
+  };
+
+  isFormEmpty = ({ username, email, password }) => {
+    return !username || !email || !password;
+  };
+
   render() {
     return (
-      <div>Signup</div>
+      <Container>
+        <Box
+          dangerouslySetInlineStyle={{
+            __style: {
+              backgroundColor: '#ebe2da'
+            }
+          }}
+          margin={4}
+          padding={4}
+          shape="rounded"
+          display="flex"
+          justifyContent="center"
+        >
+          <form
+            style={{
+              display: 'inlineBlock',
+              textAlign: 'center',
+              maxWidth: 450
+            }}
+            onSubmit={this.handleSubmit}
+          >
+            <Box
+              marginBottom={2}
+              display="flex"
+              direction="column"
+              alignItems="center"
+            >
+              <Heading color="midnight">Let's Get Started</Heading>
+              <Text italic color="orchid">Sign up to order some brews!</Text>
+            </Box>
+            <TextField
+              id="username"
+              type="text"
+              name="username"
+              placeholder="Username"
+              onChange={this.handleChange}
+            />
+            <TextField
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Email Address"
+              onChange={this.handleChange}
+            />
+            <TextField
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={this.handleChange}
+            />
+            <Button color="blue" text="Submit" type="submit" inline />
+          </form>
+        </Box>
+      </Container>
     );
   }
 }
